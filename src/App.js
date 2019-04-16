3654c74c1c1f509501aed75ca3a72b44a9771bee
Akash Mishra <mernjsstack@gmail.com>
2019-04-16 16:07:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TrendPart2 from "./Component/TrendPart2";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Line Chart Assignment .</h1>
+        <TrendPart2/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
